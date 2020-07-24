aaa05223ee3022dbc3c3816173a7ec0dc921211b
jin kyung lee <jk@jinui-MacBook-Pro.local>
2020-07-24 23:35:55 +0900

    input list 컴포넌트 레이아웃 수정

--- todoapp/src/components/TodoInput.vue
+++ todoapp/src/components/TodoInput.vue
@@ -2,8 +2,8 @@
     <div class="inputWrap">
         <!-- v - model : input에서 입력된 것이 뷰단(스크립트)에서 인식이 되게 하려면 시용  -->
         <!-- 입력된 값이 동적으로 뷰 이스턴스 값에 바로바로 맵핑되게 하기 위해서 사용 -->
-        <input class="inputText" type="text" v-model="inputItemes" v-on:keyup.enter="addTodoText">
-        <button class="addBtn" v-on:click="addTodoText">+</button>
+        <input class="inputText shadow" type="text" v-model="inputItemes" v-on:keyup.enter="addTodoText" placeholder="Type what you have to do">
+        <button class="addBtn shadow" v-on:click="addTodoText">+</button> 
     </div>
 </template>
 
@@ -26,8 +26,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+*{margin: 0; padding: 0;}
+.inputWrap{
+    position: relative;
+    margin: 20px auto;
+    width: 90%;
+}
 .inputWrap .inputText{
-    width: 200px;
+    width: 100%;    
     height: 35px;
     line-height: 30px;
     font-size: 16px;
@@ -35,14 +41,14 @@ export default {
     box-sizing: border-box;
     border: 0;
     border-radius: 5px;
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
 }
 .inputWrap .inputText:focus{
     outline-offset: 0px;   
     outline: -webkit-focus-ring-color auto 0px;
 }
 .inputWrap .addBtn{
+    position: absolute;
+    right: 0;
     width: 35px;
     height: 35px;
     font-size: 18px;
@@ -55,5 +61,11 @@ export default {
     border-top-left-radius: 0;
     border-bottom-left-radius: 0;
     box-sizing: border-box;
+
+}
+.shadow{
+    -webkit-box-shadow: 1px 1px 4px -1px rgba(0,0,0,0.3);
+    -moz-box-shadow: 1px 1px 4px -1px rgba(0,0,0,0.3);
+    box-shadow: 1px 1px 4px -1px rgba(0,0,0,0.3);
 }
 </style>
--- todoapp/src/components/TodoList.vue
+++ todoapp/src/components/TodoList.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="listWrap">
         <ul>
-            <li v-for="i in dataList" v-bind:key="i">{{ i }}</li>
+            <li class="shadow" v-for="i in dataList" v-bind:key="i">
+                <i class="fa fa-check check" aria-hidden="true"></i>
+                {{ i }}
+                <i class="fa fa-trash trash" aria-hidden="true"></i>
+            </li>
         </ul>
     </div>
 </template>
@@ -9,15 +13,16 @@
 <script>
 export default {
     data : function(){
-        return{
+        return {
             dataList : []
         }
     },
     created : function() {  
-        if(localStorage.length > 0 ){
+        if(localStorage.length > 0  ){
             for (let i = 0; i < localStorage.length; i++) {
-                if( localStorage.key(i) !== "loglevel:webpack-dev-server")
-                this.dataList.push(localStorage.key(i));
+                if( localStorage.key(i) != "loglevel:webpack-dev-server" && localStorage.key(i) != ""){
+                    this.dataList.push(localStorage.key(i));
+                }
             }
         }
     }
@@ -26,13 +31,40 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+*{margin: 0; padding: 0;}
+.listWrap ul{
+    width: 90%;
+    margin: 0 auto;
+}
 .listWrap li{
     list-style: none;
     background-color: #fff;
     margin-bottom: 10px;
-    width: 90%;
-    height: 30px;
+    height: 35px;
     font-size: 16px;
-    line-height: 30px;
+    line-height: 35px;
+    text-align: left;
+    border-radius: 5px;
+}
+.check{
+    width: 30px;
+    text-align: center;
+    line-height: 35px;
+    padding: 0 5px;
+    color: #72a4f1;
+}
+.trash{
+    float: right;
+    color: #ce6363;
+    font-size: 18px;
+    line-height: 35px;
+    padding-right: 10px;
+    width: 30px;
+    text-align: center;
+}
+.shadow{
+    -webkit-box-shadow: 1px 1px 4px -1px rgba(0,0,0,0.3);
+    -moz-box-shadow: 1px 1px 4px -1px rgba(0,0,0,0.3);
+    box-shadow: 1px 1px 4px -1px rgba(0,0,0,0.3);
 }
 </style>
